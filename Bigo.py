1130b8220c5810b9357ea45a25aefca2d09d552a
j <joinjerrodtoday@gmail.com>
2026-01-03 20:56:13 -0800

    Classes and pointers

--- Bigo.py
+++ Bigo.py
@@ -1,9 +1,42 @@
-# O of n^2
+num1 = 11
+num2 = num1
+
+print("Before num2 value is updated:")
+print("num1 =", num1)
+print("num2 =", num2)
+
+print("\nnum1 points to:", id(num1))
+print("num2 points to:", id(num2))
+
+num1 = 5
+num2 = 22
+
+print("\nAfter num2 value is updated:")
+print("num1 = ", num1)
+print("num2 = ", num2)
+
+print("\nnum1 points to:", id(num1))
+print("num2 points to:", id(num2))
 
 
-# O(1)
+dict1 = {
+    "value": 11
+}
 
-def add_items(n):
-    return n + n
+dict2 = dict1
 
-add_items(5)
+print("Before value is updated:")
+print("dict1 =", dict1)
+print("dict2 =", dict2)
+
+print("\ndict1 points to:", id(dict1))
+print("dict2 points to:", id(dict2))
+
+dict2['value'] = 22
+
+print("\nAfter value is updated:")
+print("dict1 =", dict1)
+print("dict2 =", dict2)
+
+print("\ndict1 points to:", id(dict1))
+print("dict2 points to:", id(dict2))
