c7970bfd89fc422f05b75c4c6a656b30d9adb4dd
j <joinjerrodtoday@gmail.com>
2026-01-05 20:35:58 -0800

    LL.py

--- LL.py
+++ LL.py
@@ -1,15 +1,23 @@
-head = {
-    "value": 11,
-    "next": {
-        "value": 3,
-        "next": {
-            "value": 23,
-            "next": {
-                "value": 7,
-                "next": None
-            }
-        }
-    }
-}
 
-print(head['next']['next']['value'])
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+    
+class LinkedList:
+    def __init__(self, value): 
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+      
+
+my_linked_list = LinkedList(4)
+
+print(my_linked_list.head.value)
