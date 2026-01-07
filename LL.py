e1839538b086c8a298e16d08b1d7fab1e89582d6
j <joinjerrodtoday@gmail.com>
2026-01-06 20:58:46 -0800

    Added pop method to LL.py

--- LL.py
+++ LL.py
@@ -28,8 +28,25 @@ class LinkedList:
         self.length += 1
         return True
       
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        pre = self.head
+        while(temp.next):
+            pre = temp 
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None 
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp.value
 
 my_linked_list = LinkedList(1)
 my_linked_list.append(2)
 
-my_linked_list.print_list()
+print(my_linked_list.pop())
+print(my_linked_list.pop())
+print(my_linked_list.pop())
