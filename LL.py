19a5f437ca2deca479f850dd88a7b6183138fe42
j <joinjerrodtoday@gmail.com>
2026-01-10 21:12:56 -0800

    added prepend function

--- LL.py
+++ LL.py
@@ -43,10 +43,21 @@ class LinkedList:
             self.head = None
             self.tail = None
         return temp.value
+    
+    def prepend(self,value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+        return True
 
-my_linked_list = LinkedList(1)
-my_linked_list.append(2)
+my_linked_list = LinkedList(2)
+my_linked_list.append(3)
 
-print(my_linked_list.pop())
-print(my_linked_list.pop())
-print(my_linked_list.pop())
+my_linked_list.prepend(1)
+
+my_linked_list.print_list()
